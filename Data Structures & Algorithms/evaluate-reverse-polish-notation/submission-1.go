56bade41a7ac8aa30758084a03726be61124a218
Rijul Sudesh <rijsud255@gmail.com>
2026-05-22 16:46:16 +0530

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.go
@@ -0,0 +1,25 @@
+func evalRPN(tokens []string) int {
+	stack := []int{}
+
+	for _, token := range tokens {
+		switch token {
+			case "+":
+				stack[len(stack)-2] += stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			case "-":
+				stack[len(stack)-2] -= stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			case "*":
+				stack[len(stack)-2] *= stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			case "/":
+				stack[len(stack)-2] /= stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			default:
+				num, _ := strconv.Atoi(token)
+				stack = append(stack, num)
+		}
+	}
+
+	return stack[0]
+}
