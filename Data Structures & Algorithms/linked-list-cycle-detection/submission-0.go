eb8bd29f4d97d9bdedbb5e53c3699cb16be53a61
Rijul Sudesh <rijsud255@gmail.com>
2026-06-01 01:17:59 +0530

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    seen := make(map[*ListNode]bool)
+    cur := head
+    for cur != nil {
+        if seen[cur] {
+            return true
+        }
+        seen[cur] = true
+        cur = cur.Next
+    }
+    return false
+}
