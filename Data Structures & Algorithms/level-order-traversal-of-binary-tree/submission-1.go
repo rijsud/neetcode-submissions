0cbc55190a9e63dd27f174f5b034d5efc1a35930
Rijul Sudesh <rijsud255@gmail.com>
2026-07-17 11:32:34 +0530

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func levelOrder(root *TreeNode) [][]int {
+    res := [][]int{}
+	if root == nil {return res}
+
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		list := []int{}
+		qLen := len(queue)
+		for i := 0; i < qLen; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			list = append(list, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		res = append(res, list)
+	}
+
+	return res
+}
