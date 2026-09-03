a2db2e89dd889ab8d6853eb3abd116830c627ec0
Rijul Sudesh <rijsud255@gmail.com>
2026-09-03 12:47:33 +0530

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.go
@@ -0,0 +1,21 @@
+func validTree(n int, edges [][]int) bool {
+    allEdges := make([][]int, n)
+	visit := map[int]bool{}
+	for _, edge := range edges {
+		allEdges[edge[0]] = append(allEdges[edge[0]], edge[1])
+		allEdges[edge[1]] = append(allEdges[edge[1]], edge[0])
+	}
+
+	var dfs func(int, int) bool
+	dfs = func(parent int, node int) bool {
+		if visit[node] {return false}
+		visit[node] = true
+		for _, nei := range allEdges[node] {
+			if nei == parent {continue}
+			if !dfs(node, nei) {return false}
+		}
+		return true
+	}
+
+	return dfs(-1, 0) && len(visit) == n
+}
