5087f05b10fe84e30184933f49fa4676dc76b733
Rijul Sudesh <rijsud255@gmail.com>
2026-09-14 10:28:06 +0530

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.go
@@ -0,0 +1,7 @@
+func minCostClimbingStairs(cost []int) int {
+    // cost = append(cost, 0)
+	for i := len(cost) - 3; i > -1; i-- {
+		cost[i] += min(cost[i + 1], cost[i + 2])
+	}
+	return min(cost[0], cost[1])
+}
